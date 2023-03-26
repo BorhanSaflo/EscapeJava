ce5782a7265e2d71dabbbfb5499358f066c10c59
lailaalbalkhi <albalkhi.laila@gmail.com>
2023-03-26 01:13:03 -0400

    removing unnecessary comments

--- src/createObjects.java
+++ src/createObjects.java
@@ -39,10 +39,6 @@ public class createObjects {
         roomBG.addChild(createObject("+middletable", new AxisAngle4d(0, 1, 0, Math.PI/2), new Vector3d(0.16, -0.103, 0.125), 0.06));
         roomBG.addChild(createObject("+middletable", new AxisAngle4d(0, 1, 0, Math.PI/2), new Vector3d(0.16, -0.103, 0.45), 0.06));
 
-        // roomBG.addChild(createObject("@blueBin",  new AxisAngle4d(0, 0, 0, Math.PI/2),  new Vector3d(-0.23, -0.089, 0.54), 0.2));
-        // roomBG.addChild(createObject("@redBin",   new AxisAngle4d(0, 0, 0, Math.PI/2),  new Vector3d(-0.23, -0.089, 0.6), 0.2));
-        // roomBG.addChild(createObject("@blackBin", new AxisAngle4d(0, 0, 0, Math.PI/2),  new Vector3d(-0.23, -0.089, 0.66), 0.2));
-
         roomBG.addChild(createObject("@blueBin",  new AxisAngle4d(0, 0, 0, Math.PI/2),  new Vector3d(-0.27, -0.103, 0.70), 0.15));
         roomBG.addChild(createObject("@redBin",  new AxisAngle4d(0, 0, 0, Math.PI/2),  new Vector3d(-0.27, -0.103, 0.745), 0.15));
         roomBG.addChild(createObject("@blackBin",  new AxisAngle4d(0, 0, 0, Math.PI/2),  new Vector3d(-0.27, -0.103, 0.79), 0.15));
