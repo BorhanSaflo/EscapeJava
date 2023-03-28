707393c3bcf07bca6ec4752b9f4eef4adcacf218
tanzimfh <tanzimfh@gmail.com>
2023-03-28 01:32:32 -0400

    sHoRteN fAn

--- src/createObjects.java
+++ src/createObjects.java
@@ -427,6 +427,12 @@ public class createObjects {
                 objTG.setName(name);
                 objTG.setUserData(transform);
                 objTG.addChild(new Box(x, y, z, Primitive.GENERATE_NORMALS, appearance));
+
+                if(name.equals("redClue")){
+
+                }
+
+
                 return objTG;
         }
 
@@ -434,6 +440,7 @@ public class createObjects {
                 BranchGroup BG = new BranchGroup();
         
                 BG.addChild(createBox("redClue", new AxisAngle4d(0, 0, 0, 0), new Vector3d(0.4, -0.06, -0.065), 0.1f, 0.01f, 0.1f, 0.05f, LoadObject.obj_Appearance(Red)));
+
         
                 return BG;
         }
