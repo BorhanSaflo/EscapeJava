7db516b601e7464ca5cccc033ff409bd7d195d61
Borhan Saflo <60056206+BorhanSaflo@users.noreply.github.com>
2023-04-01 04:43:28 -0400

    fix: Chairs Rotatable Before Computers Puzzle

--- src/ChairsPuzzle.java
+++ src/ChairsPuzzle.java
@@ -14,41 +14,43 @@ public class ChairsPuzzle {
     }
 
     public static void rotateChair(TransformGroup chairTG) {
-        if (unlocked || !isUsable)
-            return;
-
         double angle = (double) chairTG.getUserData();
 
-        switch (chairTG.getName().charAt(chairTG.getName().length() - 1)) {
-            case '1':
-                if (angle == Math.PI) { // North-East
-                    System.out.println("Chair 1 is now in the correct position");
-                    firstChair = true;
-                } else
-                    firstChair = false;
-                break;
-            case '2':
-                if (angle == Math.PI * 3 / 2) { // North-West
-                    System.out.println("Chair 2 is now in the correct position");
-                    secondChair = true;
-                } else
-                    secondChair = false;
-                break;
-            case '3':
-                if (angle == Math.PI / 2) { // South-East
-                    System.out.println("Chair 3 is now in the correct position");
-                    thirdChair = true;
-                } else
-                    thirdChair = false;
-                break;
-            default:
-                break;
-        }
+        if (unlocked)
+            return;
 
-        if (firstChair && secondChair && thirdChair) {
-            unlocked = true;
-            System.out.println("All chairs are in the correct position");
-            Sounds.playSound(Sounds.successSound);
+        if (isUsable) {
+            switch (chairTG.getName().charAt(chairTG.getName().length() - 1)) {
+                case '1':
+                    if (angle == Math.PI) { // North-East
+                        System.out.println("Chair 1 is now in the correct position");
+                        firstChair = true;
+                    } else
+                        firstChair = false;
+                    break;
+                case '2':
+                    if (angle == Math.PI * 3 / 2) { // North-West
+                        System.out.println("Chair 2 is now in the correct position");
+                        secondChair = true;
+                    } else
+                        secondChair = false;
+                    break;
+                case '3':
+                    if (angle == Math.PI / 2) { // South-East
+                        System.out.println("Chair 3 is now in the correct position");
+                        thirdChair = true;
+                    } else
+                        thirdChair = false;
+                    break;
+                default:
+                    break;
+            }
+
+            if (firstChair && secondChair && thirdChair) {
+                unlocked = true;
+                System.out.println("All chairs are in the correct position");
+                Sounds.playSound(Sounds.successSound);
+            }
         }
 
         angle += Math.PI / 4;
